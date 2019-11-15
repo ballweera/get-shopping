46ebcbb6fbec6fda71dbaf5434d96f2604d43e9d
ballweera <wkasetsin@gmail.com>
2019-11-15 21:44:13 +0700

    need to deal with import cycle

---
+++ cmd/shopping-api/internal/handler/cart.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/ballweera/get-shopping/service"
+)
+
+type CartController struct {
+	CartService service.CartService
+}
+
+func (this *CartController) GetCartId(w http.ResponseWriter, r *http.Request) {
+}
+
+func (this *CartController) GetCartItems(w http.ResponseWriter, r *http.Request) {
+}
